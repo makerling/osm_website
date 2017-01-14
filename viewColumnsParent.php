d455fa67bf2373547ab3f1c4908e7fb7085689ce
Baggerone <d7e5fbb0@opayq.com>
2017-01-14 08:37:33 -0500

    added fourth iframe and call to css

--- viewColumnsParent.php
+++ viewColumnsParent.php
@@ -7,27 +7,33 @@
 /******************************************************************************/
 
 require "config.php";
-require "head.php";
+require "head.php"; 
 require "columnsScript.php";
 
 echo "
-  <script language=JavaScript>
-  " . $jsSetStyles . "  
-  </script>
+    <script language=JavaScript>
+       document.getElementById('content').id = 'viewColumnsContent';
+    </script>
+";
 
-  <table class=\"viewColumns\">
-   <tr>
-    <td>
-     <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
-    </td>
-    <td>
-     <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
-    </td>
-    <td>
-     <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
-    </td>
-   </tr>
-  </table>
+echo "
+  <link type=\"text/css\" rel=\"stylesheet\" href=\"columns.css?v=20170106\">
+   <table class=\"viewColumns\">
+    <tr>
+     <td>
+      <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
+     </td>
+     <td>
+      <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
+     </td>
+     <td>
+      <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
+     </td>
+     <td>
+      <iframe src='viewColumns.php?iso=" . $sec_code . "' width=100%; height=100%;></iframe>
+     </td>
+    </tr>
+   </table>
  </div>
 </body>
 
